a0816d37a264f90a47b4889fa6cb8dc461fea2d7
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-12 18:04:29 +0200

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useDebugValue } from 'react'
 import './App.css'
 import './styles/global.css'
+import Counter from './components/Counter'
 
 class App extends React.Component {
   constructor () {
@@ -29,9 +30,12 @@ class App extends React.Component {
     return (
       <>
         <h1>Counter</h1>
-        <h2>{this.state.count}</h2>
-        <button className='btnMin' onClick={this.handleClickMin}>-</button>
-        <button className='btnPlus' onClick={this.handleClickPlus}>+</button>
+        <Counter 
+          count = {this.state.count}
+          substract = {this.handleClickMin}
+          increment = {this.handleClickPlus}
+          
+        />
       </>
     )
   }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,14 @@
-// import React from 'react'
+import React from 'react'
 
-// class Counter extends React.Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h2>{this.props.count}</h2>
-//                 <button className='btnMin' onClick={this.props.increment}>-</button>
-//                 <button className='btnPlus' onClick={this.props.substract}>+</button>
-//             </div>
-//         )
-//     }
-// }
-// export default Counter
+class Counter extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>{this.props.count}</h2>
+                <button className='btnMin' onClick={this.props.substract}>-</button>
+                <button className='btnPlus' onClick={this.props.increment}>+</button>
+            </div>
+        )
+    }
+}
+export default Counter
