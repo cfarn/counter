d65489b415fb09f7d7bf501dc4a6445ad3b6d8b7
cfarn <58476975+cfarn@users.noreply.github.com>
2022-07-12 17:49:53 +0200

    1

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+// class Counter extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h2>{this.props.count}</h2>
+//                 <button className='btnMin' onClick={this.props.increment}>-</button>
+//                 <button className='btnPlus' onClick={this.props.substract}>+</button>
+//             </div>
+//         )
+//     }
+// }
+// export default Counter
